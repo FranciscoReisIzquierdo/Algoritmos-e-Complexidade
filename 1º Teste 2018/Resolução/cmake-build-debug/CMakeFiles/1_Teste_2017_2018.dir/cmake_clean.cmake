6be6b0b1f335922ad952273ec83a035e087f1687
Francisco Reis Izquierdo A93241 <62063067+FranciscoReisIzquierdo@users.noreply.github.com>
2021-02-06 21:02:53 +0000

    Enunciado/Resolução do teste

---
+++ 1º Teste 2018/Resolução/cmake-build-debug/CMakeFiles/1_Teste_2017_2018.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1_Teste_2017_2018.exe"
+  "1_Teste_2017_2018.exe.manifest"
+  "1_Teste_2017_2018.pdb"
+  "CMakeFiles/1_Teste_2017_2018.dir/main.c.obj"
+  "lib1_Teste_2017_2018.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/1_Teste_2017_2018.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
