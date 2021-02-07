9097f84daca370403c28e716056e9ef13beb29f8
Francisco Reis Izquierdo A93241 <62063067+FranciscoReisIzquierdo@users.noreply.github.com>
2021-02-07 17:42:43 +0000

    Enunciado/Resolução do teste

---
+++ Recurso 2017/Resolução/cmake-build-debug/CMakeFiles/Recurso_2019_2020.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Recurso_2019_2020.dir/main.c.obj"
+  "Recurso_2019_2020.exe"
+  "Recurso_2019_2020.exe.manifest"
+  "Recurso_2019_2020.pdb"
+  "libRecurso_2019_2020.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Recurso_2019_2020.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
